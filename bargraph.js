5e5ac2e955b0ea417fd6e2467a87b745d4f4dc9d
lydiab72 <132516418+lydiab72@users.noreply.github.com>
2023-10-02 21:53:16 -0600

    Update bargraph.js

--- bargraph.js
+++ bargraph.js
@@ -1,131 +1,40 @@
-// Use the D3 library to read in samples.json
-d3.json("Project 3/final_data.json").then(function(data) {
-    console.log(data);
-  });
+// Create an object to store the count of campsites per category
+const campsiteCounts = {};
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    console.log(data);
-    
-    let xData = [data, 'XXX', { key: 'XXX' }, true];
-    console.log(xData);
+// Loop through the data and count campsites per category
+data.forEach(entry => {
+  const campsiteCategory = entry.Campsite; // Get the campsite category
+  if (campsiteCategory) {
+    // If the category exists, increment the count by 1, or initialize to 1
+    campsiteCounts[campsiteCategory] = (campsiteCounts[campsiteCategory] || 0) + 1;
+  }
 });
 
- //Function that Display each key-value pair from the metadata
-function demo_info(sample){
-    d3.json(url).then(function(data) {
-        let mdata=data.metadata;
-        let res=mdata.filter(function(sample_res){return sample_res.id==sample});
-        //let res=mdata.filter(sample_res => sample_res.id==sample)
-        let data_res=res[0];
-        //console.log(data_res)
-        d3.select("#sample-metadata").html("");
-        Object.entries(data_res).forEach(function([key,value]){
-            d3.select("#sample-metadata")
-            .append("p").text(`${key}:${value}`);
-
-        });
-
-    });
+// Print the campsite counts per category
+for (const category in campsiteCounts) {
+  if (campsiteCounts.hasOwnProperty(category)) {
+    console.log(`${category}: ${campsiteCounts[category]}`);
+  }
 }
 
-//bar chart function
-function bar_building(sample){
-    d3.json(url).then(function(data) {
-        let sampleData=data.samples;
-        //let res=mdata.filter(function(sample_res){sample_res.id==sample});
-        let res=sampleData.filter(sample_res => sample_res.id==sample)
-        let data_res=res[0];
-        
-        //get the otu labels, values and ids
-        let otu_ids=data_res.otu_ids;
-        let otu_labels=data_res.otu_labels;
-        let sample_values=data_res.sample_values;
-        
+// Campsite names
+const names = Object.keys(campsiteCounts); // Use Object.keys to get category names
 
-        //barchart
-        let yticks=otu_ids.slice(0,10).map(function(id){
-            return `OTU ${id}`
-        });
+// Trace for Data
+const trace1 = {
+  x: names, // Use the 'names' array for x-axis
+  y: Object.values(campsiteCounts), // Use Object.values to get category counts
+  type: "bar"
+};
 
-        let x_values=sample_values.slice(0,10);
-        let text_labels=otu_labels.slice(0,10);
+// Data trace array
+const traceData = [trace1];
 
-        let bar_chart={
-            y:yticks.reverse(),
-            x:x_values.reverse(),
-            text:text_labels.reverse(),
-            type:"bar",
-            orientation:"h",
-            mode:"markers",
-            marker:{
-                
-                color:x_values,
-                colorscale:"#FF4F00"
-            }
-        }
+// Apply the group barmode to the layout
+const layout = {
+  title: "Campsites"
+  
+};
 
-        let layout={
-            margin: { t: 55, r: 25, l: 65, b: 25 },
-            title:"Top 10 Belly Button Bacteria"
-        };
-        var config = {responsive: true}
-
-        Plotly.newPlot("bar",[bar_chart],layout,config);
-
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Fetch the JSON data and console log it
-function initialize(){
-    
-    var select=d3.select("#selDataset");
-    d3.json(url).then(function(data) {
-        let sample_names=data.names;
-        console.log(sample_names)
-
-        //making options for samples
-        sample_names.forEach(function(sample) {
-            select.append("option")
-            .text(sample)
-            .property("value",sample)
-        });
-        //let sample1=sample_names[0]; UPDATE BELOW
-        demo_info(sample1);
-        bar_building(sample1);
-        bubble_building(sample1);
-        Gauge_building(sample1);
-    });
-
-    
-}
-
-
-// Function for dashboard updating
-// THIS IS WHERE WE PUT GRAPH/MAP INPUTS
-function optionChanged(item){
-    demo_info(item)
-    bar_building(item)
-    bubble_building(item)
-    Gauge_building(item)
-}
-
-initialize();
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", traceData, layout);
